0030cc3d95a2592c3847bfed724916e6faebabe0
Johanna Farace <Johanna.farace@gmail.com>
2020-05-20 18:08:27 +0200

    index js

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+// app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/stylesheets'));
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log("I'm lstening!");
+});
